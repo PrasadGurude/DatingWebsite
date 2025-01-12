e10ce600f0e84d74cc118fdbc24d501d8f7ae022
Prasad Gurude <prasadgurude76@gmail.com>
2025-01-12 11:14:57 +0530

    work start

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,18 +2,25 @@ import { useState } from 'react'
 
 import './App.css'
 import Header from './components/Header'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Profile from './components/Profile'
+import All from './components/All'
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
 
   return (
     <div className="App">
-      <Header />
       <BrowserRouter>
+        <Header />
         <Routes>
-          <Route path='/home' Component={}></Route>
-          <Route></Route>
-          <Route></Route>
+          <Route path='/' element={<Navigate to='/home' />}></Route>
+          <Route path='/home' element={<Home />}></Route>
+          <Route path='/about' element={<About />}></Route>
+          <Route path='/profile' element={isAuthenticated ? <Profile /> : <Navigate to='/home' />}></Route>
+          <Route path='/all-students' element={isAuthenticated ? <All /> : <Navigate to='/home' />}></Route>
         </Routes>
       </BrowserRouter>
 
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../assets/logo.jpg'
 
-const Header: React.FC = () => {
+interface HeaderProps {
+  status: boolean;
+}
+
+const Header: React.FC<HeaderProps> = ({status}) => {
   return (
     <header className="bg-pink-500 text-white p-4 flex justify-between items-center">
       <div className="flex items-center">
-        <img src="/path/to/logo.png" alt="Logo" className="h-10 mr-4" />
+        <img src={logo} alt="Logo" className="h-10 mr-4 rounded-lg" />
         <nav className="flex space-x-4">
           <Link to="/home" className="hover:text-pink-200">Home</Link>
           <Link to="/about" className="hover:text-pink-200">About</Link>
           <Link to="/all-students" className="hover:text-pink-200">All Students</Link>
+          <Link to="/profile" className="hover:text-pink-200 mr-4">Profile</Link>
         </nav>
       </div>
       <div className="flex items-center">
-        <Link to="/profile" className="hover:text-pink-200 mr-4">Profile</Link>
-        <img src="/path/to/profile-image.jpg" alt="Profile" className="h-10 w-10 rounded-full" />
+        <div className=''>
+          <Link to="/login" className="hover:text-pink-200">Login or singup</Link>
+        </div>
       </div>
     </header>
   );
